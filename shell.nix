5856eac9ee57763cbd734424c0dc813196ce24da
ralismark <13449732+ralismark@users.noreply.github.com>
2025-10-05 00:18:51 +1000

    Use latest nixpkgs for latest yt-dlp

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 let
-  pkgs = import <nixpkgs> {};
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixpkgs-unstable";
+  pkgs = import nixpkgs {};
   HERE = builtins.toString ./.;
 in
 
